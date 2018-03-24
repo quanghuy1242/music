dddcf2dc956d9e09f13df2531b7352ff08ddaf7f
quanghuy1242 <katygaga124@gmail.com>
2018-03-24 16:42:28 +0700

    Trang đầu tiên

---
+++ js/javascript.js
@@ -0,0 +1,49 @@
+var slideIndex = 1;
+var slides = document.getElementsByClassName("imggg");
+for (var i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("imggg");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// NUT SEARCH
+function Nutsearch() {
+  var x = document.getElementById("subs");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else x.style.display = "none";
+}
+
+//tab
+function opentab(evt, tabname) {
+
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabname).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
