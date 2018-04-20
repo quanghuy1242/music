7d4de01a31f2555ea1e9c20bcb6315c89dc4150b
quanghuy1242 <katygaga124@gmail.com>
2018-04-20 10:58:11 +0700

    <3

--- js/javascript.js
+++ js/javascript.js
@@ -27,9 +27,9 @@ function Nutsearch() {
   var x = document.getElementsByClassName("sub");
   if (x[0].style.display == "none") {
     x[0].style.display = "block";
-  } else{
+  } else {
     x[0].style.display = "none";
-  } 
+  }
 }
 //tab
 function opentab(evt, tabname) {
@@ -50,11 +50,46 @@ function opentab(evt, tabname) {
 function themthongtinngsi() {
   var more = document.getElementById("hiddeninfo");
   var morex = document.getElementById("themthongtin");
-  if(more.style.display == "none"){
+  if (more.style.display == "none") {
     more.style.display = "block";
-    morex.value = morex.value.replace("Xem thêm","Ẩn bớt");
-  }else{
+    morex.value = morex.value.replace("Xem thêm", "Ẩn bớt");
+  } else {
     more.style.display = "none";
-    morex.value = morex.value.replace("Ẩn bớt","Xem thêm")
+    morex.value = morex.value.replace("Ẩn bớt", "Xem thêm")
   }
 }
+
+// Phâtlbum
+
+var tenbh = document.getElementsByClassName("albumdanhsach0");
+var tencs = document.getElementsByClassName("albumcasi");
+function Playnhac(i) {
+  // nhac.src = nhacs[i];
+  // nhac.play();
+  document.getElementById("tenbaihat").innerHTML = tenbh[i].innerHTML;
+  document.getElementById("tencasi").innerHTML = tencs[i].innerHTML;
+  document.getElementById("audioaudio").src = nhacs[i];
+  document.getElementById("audioaudio").autoplay = "autoplay";
+  document.getElementById("stt").innerHTML = i + 1;
+}
+
+function prev() {
+  var stt = document.getElementById("stt").innerHTML;
+  document.getElementById("tenbaihat").innerHTML = tenbh[stt - 2].innerHTML;
+  document.getElementById("tencasi").innerHTML = tencs[stt - 2].innerHTML;
+  document.getElementById("audioaudio").src = nhacs[stt - 2];
+  document.getElementById("audioaudio").autoplay = "autoplay";
+  document.getElementById("stt").innerHTML = stt - 1;
+}
+
+function next() {
+  var stt = document.getElementById("stt").innerHTML;
+  document.getElementById("tenbaihat").innerHTML = tenbh[stt].innerHTML;
+  document.getElementById("tencasi").innerHTML = tencs[stt].innerHTML;
+  document.getElementById("audioaudio").src = nhacs[stt];
+  document.getElementById("audioaudio").autoplay = "autoplay";
+  document.getElementById("stt").innerHTML = parseInt(stt) + 1;
+}
+
+document.getElementById("defaultplay").click();
+document.getElementById("audioaudio").pause();
