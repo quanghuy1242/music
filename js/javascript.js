cde9567bea3f4aec9f60fe8d3a4b8cf9a6c95f18
quanghuy1242 <katygaga124@gmail.com>
2018-05-03 22:49:35 +0700

    gom nhóm các bxh theo từng tab

--- js/javascript.js
+++ js/javascript.js
@@ -116,10 +116,10 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("imggg");
   if (n > slides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
